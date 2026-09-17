fa79341aa62aef8e16bd49fa573101744e51e8d7
Erik Sánchez <erik.sanchez.maldonado@gmail.com>
2026-09-17 16:43:37 -0600

    Corrige error en promedio.py y agrega intradas del usuario

--- promedio.py
+++ promedio.py
@@ -1,4 +1,8 @@
 def calcular_promedio(a, b, c) :
   return (a + b + c) / 3
 
-print(promedio(1, 2, 3))
+num1 = int(input("Escribe un número: "))
+num2 = int(input("Escribe un número: "))
+num3 = int(input("Escribe un número: "))
+
+print(calcular_promedio(num1, num2, num3))
