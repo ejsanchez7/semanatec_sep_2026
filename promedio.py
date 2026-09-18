281f5e807bc3e4ffa8a1935f775ceaee95e3fe8e
Erik Sánchez <erik.sanchez.maldonado@gmail.com>
2026-09-18 16:39:33 -0600

    Modifica el mensaje de promedio.py para hacerlo más entendible por el usuario e indicar qué número está introduciendo

--- promedio.py
+++ promedio.py
@@ -1,8 +1,8 @@
 def calcular_promedio(a, b, c) :
   return (a + b + c) / 3
 
-num1 = int(input("Escribe un número: "))
-num2 = int(input("Escribe un número: "))
-num3 = int(input("Escribe un número: "))
+num1 = int(input("Escribe el primer número: "))
+num2 = int(input("Escribe el segundo número: "))
+num3 = int(input("Escribe el tercer número: "))
 
 print(calcular_promedio(num1, num2, num3))
